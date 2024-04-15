2bd8173b6ae5d8413f39876c420d64dcb43eaa7c
Mark <87201176+kramhcan@users.noreply.github.com>
2024-04-16 04:25:35 +0800

    Update next.config.mjs
    For image test

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     basePath: "/kramhcan-homepage",
+    assetPrefix: "/kramhcan-homepage",
     output: "export",
     reactStrictMode: true,
 };
