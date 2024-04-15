ff49688609cc6d6e777c3ba84e4bdcbd7b9927bd
Mark <markchan0134@gmail.com>
2024-04-16 03:37:49 +0800

    config and workflw

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: "/kramhcan-homepage",
+    output: "export",
+    reactStrictMode: true,
+};
 
 export default nextConfig;
