2a0ed265e73afcda2639f58a7aebcc1fdfb0fb59
Mark <markchan0134@gmail.com>
2024-04-16 04:17:04 +0800

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    basePath: "/kramhcan-homepage",
-    output: "export",
-    reactStrictMode: true,
-};
+const nextConfig = {};
 
 export default nextConfig;
